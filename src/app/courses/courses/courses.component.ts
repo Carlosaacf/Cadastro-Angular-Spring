97e74820c8c28596dbeee8d6ad48c0cc67ed9d36
carlosaacf <carlosaacf@gmail.com>
2024-06-04 22:46:47 -0300

    inclusao da toolbar

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.css'
+  styleUrl: './courses.component.css',
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Frontend',
+    },
+    {
+      _id: '2',
+      name: 'React',
+      category: 'Frontend',
+    },
+    {
+      _id: '3',
+      name: 'Node',
+      category: 'Backend',
+    },
 
+  ];
+
+  displayedColumns = ['name', 'category'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
